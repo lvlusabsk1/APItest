05c4e9f66c92141bf0a9d8abb863f9be83e07c0e
Musab <113135560+lvlusabsk1@users.noreply.github.com>
2025-02-27 22:07:38 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CarSearch from "./components/CarSearch";
+
+function App() {
+  return (
+    <div>
+      <h1>Car API Website</h1>
+      <CarSearch />
+    </div>
+  );
+}
+
+export default App;
